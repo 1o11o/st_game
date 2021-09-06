c33eb184e9619f063aa15f5dc660d10780b08085
lorenz <lorenz.reiter@posteo.at>
2021-09-06 11:57:47 +0200

    sec attempt

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# This is a sample Python script.
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name = 'lollo'
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
